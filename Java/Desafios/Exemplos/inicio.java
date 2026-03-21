52e21c912186fca94c839d15ba2a84049c478254
Miguel Arcanjo <miguelarcanjomartinsilva@gmail.com>
2026-03-21 18:01:55 -0300

    removendo pasta Java para subtree

--- Java/Desafios/Exemplos/inicio.java
+++
@@ -1,7 +0,0 @@
-package Exemplos;
-public class inicio {
-    public static void main(String[]args) {
-
-        System.out.print("Olá Mundo!");
-    }
-}
--- Java/Desafios/Exemplos/para.java
+++
@@ -1,9 +0,0 @@
-package Exemplos;
-public class para {
-    public static void main(String[] args) {
-        for (int i = 1; i <=5; i++) {
-
-            System.out.println("Repetir número: " + i);
-        }
-    }
-}
